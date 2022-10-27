7fec6d7eab4cfcc6fa8c0a2ac4bc2020c7378844
luciano060999martinez <luciano.martinez060999@gmail.com>
2022-10-27 11:49:20 -0300

    Base de datos conectada

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_trazabilidad',
+        'USER': 'trazabilidad',
+        'PASSWORD': 'trazabilidad2022',
+        'HOST': 'localhost',
+        'PORT':''
     }
 }
 
