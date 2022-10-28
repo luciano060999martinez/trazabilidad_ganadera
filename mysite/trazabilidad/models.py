d25c0531a037788c06091d680b77095867619958
luciano060999martinez <luciano.martinez060999@gmail.com>
2022-10-28 14:32:04 -0300

    Creacion de los modelos vacuna, vacunaXanimal, se anhadio FK en algunos modelos

--- mysite/trazabilidad/models.py
+++ mysite/trazabilidad/models.py
@@ -25,7 +25,8 @@ class Propietario(models.Model):
 
 class Estado(models.Model):
     #por defecto crea estado_id
-    estado_animal = models.CharField(max_length = 1, choices = estados, default = 'V')
+    estado_animal = models.CharField(max_length = 1, choices = estados)
+
 
 class Animal(models.Model):
     #por defecto crea el ID ANIMAL
@@ -33,4 +34,16 @@ class Animal(models.Model):
     raza = models.CharField(max_length=50)
     peso = models.FloatField()
     propietario = models.ForeignKey(Propietario, on_delete = models.CASCADE)
+    estado = models.ForeignKey(Estado, on_delete = models.CASCADE, null = True) #recien agregado
+
+class Vacuna(models.Model): #recien agregado
+    vacuna_nombre = models.CharField(max_length = 50)
+    vacuna_origen = models.CharField(max_length = 50)
+    vacuna_dosis = models.PositiveBigIntegerField(default = 0)
+
+class Vacuna_X_Animal(models.Model): #recien agregado
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    vacuna = models.ForeignKey(Vacuna, on_delete = models.CASCADE)
+
+
 
