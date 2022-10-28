2d0d79f64059d5c7dd80c078f7145e276954accb
luciano060999martinez <luciano.martinez060999@gmail.com>
2022-10-28 00:36:58 -0300

    Se anhadio modelos de prueba, y el requirements.txt  (rama luciano)

---
+++ mysite/trazabilidad/choices.py
@@ -0,0 +1,10 @@
+sexos = (
+    ('F', 'Femenino'),
+    ('M', 'Masculino')
+)
+
+estados = (
+    ('M', 'Muerto'),
+    ('V', 'Vivo'),
+    ('P', 'En produccion')
+)
--- mysite/trazabilidad/models.py
+++ mysite/trazabilidad/models.py
@@ -1,5 +1,6 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
-
+from .choices import sexos, estados
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,3 +11,26 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Propietario(models.Model):
+    #por defecto crea el ID Propietario
+    propietario_nombre = models.CharField(max_length=50)
+    propietario_apellido = models.CharField(max_length=50)
+    propietario_ci = models.PositiveIntegerField(null=False, unique = True)
+    propietario_ruc = models.CharField(max_length = 50)
+    propietario_direccion = models.CharField(max_length = 100)
+    propietario_telefono = models.CharField(max_length = 50)
+    sexo = models.CharField(max_length = 1, choices = sexos, default = 'M')
+
+class Estado(models.Model):
+    #por defecto crea estado_id
+    estado_animal = models.CharField(max_length = 1, choices = estados, default = 'V')
+
+class Animal(models.Model):
+    #por defecto crea el ID ANIMAL
+    codigo_animal = models.CharField(max_length=50)
+    raza = models.CharField(max_length=50)
+    peso = models.FloatField()
+    propietario = models.ForeignKey(Propietario, on_delete = models.CASCADE)
+
