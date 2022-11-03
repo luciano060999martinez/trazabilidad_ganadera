9b0edc938494fb8ff7b5f04100e6a556342e6eeb
luciano060999martinez <luciano.martinez060999@gmail.com>
2022-11-03 15:06:51 -0300

    se elimino estado

--- mysite/trazabilidad/admin.py
+++ mysite/trazabilidad/admin.py
@@ -8,21 +8,8 @@ class AnimalAdmin(admin.ModelAdmin):
     search_fields = ('id',)
     #list_editable = ('estado',)
 
-@admin.register(models.Animal)
-class AnimalAdmin(admin.ModelAdmin):
-    list_display = ('codigo_animal', 'estado_animal', 'propietario')
-    search_fields = ('id',)
-    #list_editable = ('estado',)
-
- #   @admin.display()
- #   def estado(self,obj):
- #       return obj.estado.estado_animal
 
 @admin.register(models.Propietario)
 class PropietarioAdmin(admin.ModelAdmin):
     list_display = ('propietario_nombre', 'propietario_apellido', 'propietario_ci')
 # Register your models here.
-
-@admin.register(models.Estado)
-class EstadoAdmin(admin.ModelAdmin):
-    pass
